68bc8cc9954c28d3c5ae4d586def1066ba0e50cc
VictorySolo <victorysolovyova@gmail.com>
2024-12-17 23:32:23 +0200

    Updated specialists routes

--- backend/routes/specialistRoute.js
+++ backend/routes/specialistRoute.js
@@ -1,33 +1,33 @@
 const express = require('express');
 const {
     registerSpecialist,
+    getSpecialistProfile,
+    updateSpecProfile,
     appointmentsToSpec,
     appointmentCancel,
     specialistList,
     changeAvailablity,
     appointmentComplete,
-    dashboard,
-    specialistProfile,
-    updateSpecProfile } = require('../controllers/specialistController.js');
+    specialistDashboard,
+} = require('../controllers/specialistController.js');
 const {
     login,
     decodeToken,
-    logout } = require('../services/authentication.js')
-const specialistRouter = express.Router();
+ } = require('../services/authentication.js')
+const specialistRoutes = express.Router();
 
-specialistRouter.post("/sign_up", registerSpecialist);
-specialistRouter.post("/login", login)
-specialistRouter.post("/", decodeToken, logout)
-
-specialistRouter.post("/cancel-appointment", decodeToken, appointmentCancel)
-specialistRouter.get("/appointments", decodeToken, appointmentsToSpec)
-specialistRouter.get("/list", specialistList)
-specialistRouter.post("/change-availability", decodeToken, changeAvailablity)
-specialistRouter.post("/complete-appointment", decodeToken, appointmentComplete)
-specialistRouter.get("/dashboard", decodeToken, dashboard)
-specialistRouter.get("/profile", decodeToken, specialistProfile)
-specialistRouter.post("/update-profile", decodeToken, updateSpecProfile)
-
+specialistRoutes.post("/register", registerSpecialist);
+specialistRoutes.post("/login", login)
+// specialistRoutes.post("/", decodeToken, logout)
+specialistRoutes.post("/cancel_appointment", decodeToken, appointmentCancel)
+specialistRoutes.get("/appointments", decodeToken, appointmentsToSpec)
+specialistRoutes.get("/list", specialistList)
+specialistRoutes.post("/change_availability", decodeToken, changeAvailablity)
+specialistRoutes.post("/complete_appointment", decodeToken, appointmentComplete)
+specialistRoutes.get("/dashboard", decodeToken, specialistDashboard)
+specialistRoutes.get("/profile", decodeToken, getSpecialistProfile)
+specialistRoutes.post("/update_profile", decodeToken, updateSpecProfile)
 
 
-module.exports = specialistRouter
+
+module.exports = specialistRoutes
